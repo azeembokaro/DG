0070cb90f2412d55c43da59552a3e36567a96bc8
azeembokaro <razeem2015@gmail.com>
2021-11-02 17:17:02 +0530

    First Commit

---
+++ src/Clock.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+const Digital = () => {
+let Time = new Date().toLocaleTimeString();
+const [ctime,setCtime] = useState(Time);
+const UpdateTime = () => {
+    Time = new Date().toLocaleTimeString();
+    setCtime(Time);
+};
+setInterval(UpdateTime,1000);
+return (
+<>
+<h1>{ctime}</h1>
+</>
+);
+};
+
+export default Digital;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Digital from './Clock';
+ReactDOM.render(<Digital/>,document.getElementById('root'));
